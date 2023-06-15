4aa58bc0be6d1cf34a1c53744689b2545e26c20e
Joeeazy <joeeazy34@gmail.com>
2023-06-15 05:57:30 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main (void)
+/**
+  *main - puts info
+  *Return: Returns 0
+  */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
