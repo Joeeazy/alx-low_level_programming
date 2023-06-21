479e08d301796fda2b75fe08412321c12b5d546b
Joeeazy <joeeazy34@gmail.com>
2023-06-21 06:03:27 -0700

    _putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,16 @@
 #include "main.h"
+
 /**
-  * main - print alphabet
-  *Return: 
-  */
+ * print_alphabet - print all alphabet in lowercase
+ */
 
 void print_alphabet(void)
 {
 	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
+
