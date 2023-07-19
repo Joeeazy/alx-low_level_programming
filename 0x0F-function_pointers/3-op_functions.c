740233272c437991be064679a47e133fa209faa5
Joeeazy <joeeazy34@gmail.com>
2023-07-19 13:00:50 -0700

    program that performs simple operations.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stddef.h>
+
+/**
+ * op_add - function that finds the sum of two numbers.
+ * @a: The 1st num
+ * @b: The 2nd num
+ * Return: The sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function that finds the difference of two numbers.
+ * @a: The 1st num.
+ * @b: The 2nd Num.
+ * Return: The difference btn a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function that multiplies two numbers.
+ * @a: The 1st Num
+ * @b: The 2nd Num
+ * Return: The product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that divides two numbers.
+ * @a: The 1st Num
+ * @b: The 2nd Num
+ * Return: The division of a and b.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - functuion that finds the remainder after division
+ * @a: The 1st Num
+ * @b: The 2nd Num
+ *Return: The modulus
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
