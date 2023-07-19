37817b0f6442c701c5b4f9fbbbceae517c52d0b5
Joeeazy <joeeazy34@gmail.com>
2023-07-19 13:14:24 -0700

    program that performs simple operations.

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "3-calc.h"
 #include <stddef.h>
-
+#include <stdlib.h>
+#include "function_pointers.h"
 /**
  * op_add - function that finds the sum of two numbers.
  * @a: The 1st num
@@ -47,6 +48,11 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
@@ -59,6 +65,11 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
 
