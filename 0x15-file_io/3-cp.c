aecadf6ce774a009f6e00529a3695b26599e6655
Joeeazy <joeeazy34@gmail.com>
2023-09-05 01:02:46 +0300

    C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,67 +1,53 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include "main.h"
 
-/**
- * main - copies content of a file to another file
- * @argc: argument count
- * @argv: argument vector
- * Return: 97 or 98  or 99 or 100
- */
+#define READ_ERR "Error: Can't read from file %s\n"
+#define WRITE_ERR "Error: Can't write to %s\n"
 
+/**
+  * main - check the code for Holberton School students.
+  * @argc: the arguments count
+  * @argv: the arguments vector
+  * Return: Always 0.
+  */
 int main(int argc, char **argv)
 {
-int fd_in, fd_out, n, m, size, count = 0;
-char buffer[1024];
+	int from, to, on_close, w, r;
+	char buffer[1024];
 
-if (argc != 3)
-{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
-}
-fd_in = open(argv[1], O_RDONLY);
-if (fd_in == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
-}
-fd_out = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-if (fd_out == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	exit(99);
-}
-size = -1;
-while (size != 0)
-{
-	size = read(fd_in, buffer, 1024);
-	buffer[size] = '\0';
-	count += write(fd_out, buffer, size);
-	if (count == -1)
+	if (to == -1)
+		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
+	from = open(argv[1], O_RDONLY);
+
+	if (from == -1)
+		dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
+	while (1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-}
-	n = close(fd_in);
-	check_close_error(n, fd_in);
-	m = close(fd_out);
-	check_close_error(m, fd_out);
-	return (0);
-}
+		r = read(from, buffer, 1024);
 
-/**
- * check_close_error - checks for errors
- * @err: return value
- * @fd: file descriptor
- * Return: nothing
- */
-void check_close_error(int err, int fd)
-{
-if (err == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-	exit(100);
-}
+		if (r == -1)
+			dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
+		if (r > 0)
+		{
+			w = write(to, buffer, r);
+			if (w == -1)
+				dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
+		}
+		else
+			break;
+	}
+	on_close = close(from);
+	if (on_close == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
+	on_close = close(to);
+	if (on_close == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to), exit(100);
+	return (0);
 }
